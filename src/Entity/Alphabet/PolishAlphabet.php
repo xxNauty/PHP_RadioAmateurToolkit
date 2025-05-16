67f4a66e5bea54a01c6855381bd262f077dd4fcc
Mateusz Wnuk <mateusz2003w@gmail.com>
2025-05-16 13:28:09 +0200

    alphabet enums

---
+++ src/Entity/Alphabet/PolishAlphabet.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Entity\Alphabet;
+
+enum PolishAlphabet: string
+{
+    case A = "Adam";
+    case Ą = "_";
+    case B = "Barbara";
+    case C = "Celina";
+    case Ć = "Ćma";
+    case D = "Dorota";
+    case E = "Edward";
+    case Ę = "__";
+    case F = "Filip";
+    case G = "Gustaw";
+    case H = "Henryk";
+    case I = "Ignacy";
+    case J = "Józef";
+    case K = "Karol";
+    case L = "Ludwik";
+    case Ł = "Łukasz";
+    case M = "Marian";
+    case N = "Nikodem";
+    case Ń = "Koń";
+    case O = "Olga";
+    case Ó = "";
+    case P = "Paweł";
+    case R = "Roman";
+    case S = "Stefan";
+    case Ś = "Światowid";
+    case T = "Tadeusz";
+    case U = "Urszula";
+    case V = "Violetta";
+    case W = "Walenty";
+    case X = "Xawery";
+    case Y = "Ypsylon";
+    case Z = "Zygmunt";
+    case Ź = "Źrebak";
+    case Ż = "Żaba";
+}
